9afa88106a6eeddac4e6cbc2e6e4db685e11a88e
Sakib <122315943+Sakif16@users.noreply.github.com>
2026-04-14 19:30:58 +0600

    added mp4 options for Video download, renamed

--- download.py
+++ download.py
@@ -10,24 +10,43 @@ init()
 f = Figlet(font='slant')
 print(Fore.CYAN + f.renderText('YTMP3') + Style.RESET_ALL)
 
-print(Fore.GREEN + "Welcome to Media Downloader" + Style.RESET_ALL)
+print(Fore.GREEN + "Welcome to Media Downloader\n" + Style.RESET_ALL)
 
 
 #Dwonloaded files destination folder
 download_folder = os.path.join(os.path.expanduser("~"), "OneDrive", "Desktop", "downloaded mp3s")
 
 #choice for audio or video
-choice = input("Download Video [1] or Audio [2]?: ")
+print("Download-\n")
+print("[1] Video")
+print("[2] Audio\n")
+
+choice = input("Choice: ")
+
+if choice == "1":
+    print("Video format-\n")
+    print("[1]webm")
+    print("[2]mp4\n")
+    video_format_choice = input("Choice: ")
+    
 
 url = input("Paste YouTube video link: ")
 
 
 #Video download
 if choice == '1':
-    ydl_opts ={
-        "format":"bestvideo+bestaudio/best",
-        "outtmpl": os.path.join(download_folder, '%(title)s.%(ext)s')
-    }
+    if video_format_choice == "1":
+        ydl_opts ={
+            "format":"bestvideo+bestaudio/best",
+            "outtmpl": os.path.join(download_folder, '%(title)s.%(ext)s')
+        }
+    elif video_format_choice == "2":
+        ydl_opts ={
+            "format":"bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
+            "outtmpl": os.path.join(download_folder, '%(title)s.%(ext)s')
+        }
+    else: print("Invalid choice")
+
 
 #Audio download
 elif choice == "2":
