bdf2601ceffe8c9d2a49f354ef4fa5769128bf10
Sakib <122315943+Sakif16@users.noreply.github.com>
2026-04-24 16:22:29 +0600

    fixed ffmpeg requirement installations

--- octoload/main.py
+++ octoload/main.py
@@ -1,3 +1,49 @@
+def ensure_ffmpeg():
+    import os
+    import shutil
+    import urllib.request
+    import zipfile
+   
+
+    # If already installed globally → use it
+    if shutil.which("ffmpeg"):
+        return None  # yt-dlp will use system ffmpeg
+
+    print("⚠️ FFmpeg not found. Downloading automatically...\n")
+
+    home = os.path.expanduser("~")
+    ffmpeg_dir = os.path.join(home, ".octoload", "ffmpeg")
+    os.makedirs(ffmpeg_dir, exist_ok=True)
+
+    zip_path = os.path.join(ffmpeg_dir, "ffmpeg.zip")
+
+    # Windows build (you can extend later)
+    url = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
+
+    # Download
+    urllib.request.urlretrieve(url, zip_path)
+
+    # Extract
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(ffmpeg_dir)
+
+    # Find ffmpeg.exe
+    ffmpeg_path = None
+    for root, dirs, files in os.walk(ffmpeg_dir):
+        if "ffmpeg.exe" in files:
+            ffmpeg_path = root
+            break
+
+    if ffmpeg_path:
+        print("✅ FFmpeg ready!\n")
+        return ffmpeg_path
+    else:
+        print("❌ Failed to setup FFmpeg.")
+        return None
+
+
+
+
 def main():
 
     import yt_dlp
@@ -7,6 +53,8 @@ def main():
 
     # Initialized colorama
     init()
+
+    ffmpeg_location = ensure_ffmpeg()
 
     # ===== Banner =====
     f = Figlet(font='slant')
@@ -40,12 +88,14 @@ def main():
         if video_format_choice == "1":
             ydl_opts ={
                 "format":"bestvideo+bestaudio/best",
-                "outtmpl": os.path.join(download_folder, '%(title)s.%(ext)s')
+                "outtmpl": os.path.join(download_folder, '%(title)s.%(ext)s'),
+                "ffmpeg_location": ffmpeg_location
             }
         elif video_format_choice == "2":
             ydl_opts ={
                 "format":"bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
-                "outtmpl": os.path.join(download_folder, '%(title)s.%(ext)s')
+                "outtmpl": os.path.join(download_folder, '%(title)s.%(ext)s'),
+                "ffmpeg_location": ffmpeg_location
             }
         else: print("Invalid choice")
 
@@ -55,6 +105,7 @@ def main():
         ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': os.path.join(download_folder, '%(title)s.%(ext)s'),
+        "ffmpeg_location": ffmpeg_location,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -66,6 +117,11 @@ def main():
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+
+
+
+
+
 
 
 
